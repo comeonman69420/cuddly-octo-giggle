f6df2e04bfc26dc0e214e9f7139374b06f8d6956
comeonman69420 <109034636+comeonman69420@users.noreply.github.com>
2022-07-10 12:38:29 -0400

    Create guatemala.js

---
+++ guatemala.js
@@ -0,0 +1,108 @@
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+if (context.params.event.content.toLowerCase().includes(`among us`)) {
+let event = context.params.event
+let text = context.params.event.content.split(` `);
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `🏳️‍⚧️`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`cup pong`)) {
+let event = context.params.event
+let text = context.params.event.content.split(` `);
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `<:frownysobcry:983801912690356345>`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`nico`)) {
+let event = context.params.event
+let text = context.params.event.content.split(` `);
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `🐥<:frownysobcry:983801912690356345>`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`furry`)) {
+let event = context.params.event
+let text = context.params.event.content.split(` `);
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `furry? you must be referring to ilse. we all know what she gets up to with nico`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`applesauce`) || context.params.event.content.toLowerCase().includes(`apple sauce`)) {
+let event = context.params.event
+let text = context.params.event.content.split(` `);
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `We'd love to hear from you!`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`meow`)) {
+let event = context.params.event
+let text = context.params.event.content.split(` `);
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `"meow," ilse said, waking up. it was another great day to be a panther- by that, i of course mean her fursona, ~~massaponax~~ lollipop, which despite not being a panther, is. she is a furry larper. the meowing is just part of the bit. "meow," she repeated, putting on her fursuit head. nico, her pet canary, sobbed in the corner`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`women`)) {
+let event = context.params.event
+let text = context.params.event.content.split(` `);
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `cringe`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`1984`)) {
+let event = context.params.event
+let text = context.params.event.content.split(` `);
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `1984 by george orwell`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`winston`)) {
+let event = context.params.event
+let text = context.params.event.content.split(` `);
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `winston smith from 1984 by george orwell`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`ilse`)) {
+let event = context.params.event
+let text = context.params.event.content.split(` `);
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `meow`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`super ultra supreme`)) {
+let event = context.params.event
+let text = context.params.event.content.split( );
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `super ultra supreme rabies, also known as 'sus rabies' in Newspeak. Do you want sus rabies, Winston?`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`melon`)) {
+let event = context.params.event
+let text = context.params.event.content.split( );
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `(melon)`,
+  })};
+
+if (context.params.event.content.toLowerCase().includes(`sus`)) {
+let event = context.params.event
+let text = context.params.event.content.split( );
+if (context.params.event.content.toLowerCase().includes(`sus rabies`))
+pass
+else
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `sus rabies*`,
+  })};
